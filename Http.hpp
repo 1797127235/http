240e393e88602d42827c5576d429f789c019e543
questionliuxinyu <1797127235@qq.com>
2025-06-28 11:43:01 +0800

    update get file

--- Http.hpp
+++ Http.hpp
@@ -2,13 +2,20 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<unordered_map>
+#include<sstream>
+#include<fstream>
 #include"Log.hpp"
 using namespace log_ns;
 const static std::string base_sep="\r\r";
+const static std::string base_key=": ";
+const static std::string prefixpath="wwwroot";
+const static std::string home_path="index.html";
+const static std::string http_version="HTTP/1.0";
 class HttpRequest
 {
 public:
-    HttpRequest():_blank_line(base_sep)
+    HttpRequest():_blank_line(base_sep),_path(prefixpath)
     {}
     ~HttpRequest(){}
     std::string GetLine(std::string &reqstr)
@@ -24,7 +31,38 @@ public:
             return base_sep;
         }
         reqstr.erase(0,pos+base_sep.size());
-        return line;
+        return line; 
+    }
+    void ParseReqLine()//解析请求行
+    {
+        std::stringstream ss(_req_line);
+        ss>>_method>>_url>>_version;
+
+        _path+=_url;
+        if(_path.back()!='/')
+        {
+            _path+='/';
+        }
+         _path+=home_path;
+
+    }
+    void ParseReqHeaders()//解析请求头
+    {
+        for(auto &header:_req_headers)
+        {
+            auto pos=header.find(base_key);
+            if(pos==std::string::npos)
+            {
+                continue;
+            }
+            std::string key=header.substr(0,pos);
+            std::string value=header.substr(pos+base_key.size());
+            if(key.empty()||value.empty())
+            {
+                continue;
+            }
+            _headers_kv[key]=value;
+        }
     }
     void Deserialization(std::string &reqstr)
     {
@@ -43,6 +81,8 @@ public:
         {
             _body_text=reqstr;
         }
+        ParseReqLine();
+        ParseReqHeaders();
     }
     void Print()
     {
@@ -53,28 +93,66 @@ public:
         }
         std::cout<<"***"<<_blank_line;
         std::cout<<"$$$"<<_body_text<<std::endl;
+
+        std::cout<<"method:"<<_method<<std::endl;
+        std::cout<<"url:"<<_url<<std::endl;
+        std::cout<<"version:"<<_version<<std::endl;
+
+    }
+    std::string Getpath()
+    {
+        return _path;
     }
 private:
     std::string _req_line;//请求行
     std::vector<std::string> _req_headers;//请求头
     std::string _blank_line;  //空行
     std::string _body_text;  //请求体
+
+    //进一步反序列化更具体的属性字段
+    std::string _method;
+    std::string _url;
+    std::string _version;
+    std::string _path;
+    std::unordered_map<std::string,std::string> _headers_kv;
 };
 
+//响应报文
 class HttpResponse
 {
+public:
+
 private:
     std::string _status_line;//状态行
-    std::vector<std::string> _resp_headers;
+    std::unordered_map<std::string,std::string> _headers_kv;
     std::string _blank_line;
+
+    std::string _http_version;
+    std::string _status_code;
+    std::string _status_msg;
+    std::string _content_type;
     std::string _resp_body_text;
 };
+
+
 
 class HttpServer{
 public:
     HttpServer()
     {
 
+    }
+    std::string GetFileContent(std::string &path)
+    {
+        std::ifstream ifs(path.c_str());
+        if(!ifs.is_open())
+        {
+            return "";
+        }
+        std::ostringstream oss;
+        oss << ifs.rdbuf();
+        return oss.str();
+        
     }
     std::string HandlerHttpRequest(std::string &reqstr)
     {
@@ -89,8 +167,9 @@ public:
 #else
         HttpRequest req;
         req.Deserialization(reqstr);
-        req.Print();
-
+        //td::string path=req.Getpath();
+        std::string content=GetFileContent(req.Getpath());
+        std::cout<<"content length: "<<content.size()<<std::endl;
         return "";
 #endif
     }
